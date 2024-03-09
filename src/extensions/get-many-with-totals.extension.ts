69ab1d4cf253a20252a6a957dde4ff90a37aff58
koenigstag <koenigstag@gmail.com>
2024-03-09 10:13:55 +0200

    Add getMany with totals calculation

---
+++ src/extensions/get-many-with-totals.extension.ts
@@ -0,0 +1,56 @@
+import { SelectQueryBuilder } from 'typeorm/query-builder/SelectQueryBuilder';
+import { PaginationFilter } from '../types/interfaces/pagination.interface';
+import { getLimitAndOffset } from '../utils/pagination.utils';
+
+export interface ListWithTotals<Entity> {
+  list: Entity[];
+  totalCount: number | null;
+  limit: number | null;
+  offset: number | null;
+}
+
+declare module 'typeorm/query-builder/SelectQueryBuilder' {
+  interface SelectQueryBuilder<Entity> {
+    getManyWithTotals(
+      paginationFilter: PaginationFilter,
+      options?: { disableTotalsCalculation?: boolean; loadAll?: boolean }
+    ): Promise<ListWithTotals<Entity>>;
+  }
+}
+
+SelectQueryBuilder.prototype.getManyWithTotals = async function <Entity>(
+  paginationFilter: PaginationFilter,
+  options?: { disableTotalsCalculation?: boolean; loadAll?: boolean }
+) {
+  const { disableTotalsCalculation = false, loadAll = false } = options || {};
+
+  const { limit, offset } = loadAll
+    ? { limit: null, offset: null }
+    : getLimitAndOffset(paginationFilter);
+
+  const requests = [];
+
+  if (!disableTotalsCalculation) {
+    requests[0] = this.getCount();
+  } else {
+    requests[0] = Promise.resolve(null);
+  }
+
+  if (loadAll) {
+    requests[1] = this.getMany();
+  } else {
+    requests[1] = this.applyPaginationFilter(paginationFilter).getMany();
+  }
+
+  const [totalCount, list] = (await Promise.all(requests)) as [
+    number | null,
+    Entity[]
+  ];
+
+  return {
+    list,
+    totalCount: totalCount,
+    limit,
+    offset,
+  };
+};
