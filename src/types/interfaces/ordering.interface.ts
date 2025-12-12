9fb4f13568fbe30ab1c128484247607a09590bef
koenigstag <50256886+koenigstag@users.noreply.github.com>
2025-12-12 10:41:17 +0200

    Enhance OrderParam type with isReversed boolean option

--- src/types/interfaces/ordering.interface.ts
+++ src/types/interfaces/ordering.interface.ts
@@ -1,20 +1,21 @@
 import { NonFunctionPropertyNames } from "../util-types";
 
-export type OrderDirection = 'asc' | 'desc' | 'ASC' | 'DESC' | 1 | -1;
+export type OrderDirection = 'asc' | 'desc' | 'ASC' | 'DESC' | 1 | -1 | true | false;
 
 export type NullsOrder = 'first' | 'last' | 'FIRST' | 'LAST';
 
 export type OrderParam<Entity> = {
   field: NonFunctionPropertyNames<Required<Entity>> | string;
+  /** -1 = DESC, false = DESC */
   direction?: OrderDirection;
   nulls?: NullsOrder;
+  /** true = DESC, false = ASC */
+  isReversed?: boolean;
 };
 
 export type OrderType = {
   selector: string;
   order?: 'ASC' | 'DESC';
-  /** true = DESC, false = ASC */
-  isReversed?: boolean;
   nulls?: 'NULLS FIRST' | 'NULLS LAST';
 };
 
